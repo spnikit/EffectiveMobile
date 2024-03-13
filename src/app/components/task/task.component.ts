79c39f8d22fe7f4210e0014bc407a54185278327
spnikit <spnikit@gmail.com>
2024-03-13 13:36:17 +0300

    feat: add status to card

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -2,13 +2,16 @@ import { Component, input } from '@angular/core';
 import {
   MatCard,
   MatCardActions,
+  MatCardContent, MatCardFooter,
   MatCardHeader,
   MatCardSubtitle,
   MatCardTitle,
 } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
-import { Task } from '../../models/task.model';
-import {RouterLink} from "@angular/router";
+import { Task, TaskStatusEnum } from '../../models/task.model';
+import { RouterLink } from '@angular/router';
+import { MatChipsModule } from '@angular/material/chips';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-task',
@@ -21,10 +24,27 @@ import {RouterLink} from "@angular/router";
     MatCardTitle,
     MatCardSubtitle,
     RouterLink,
+    MatCardContent,
+    MatChipsModule,
+    NgClass,
+    MatCardFooter,
   ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
 export class TaskComponent {
   task = input.required<Task>();
+
+  get class() {
+    switch (this.task().status) {
+      case TaskStatusEnum.idle:
+        return 'idle';
+      case TaskStatusEnum.inProgress:
+        return 'inProgress';
+      case TaskStatusEnum.done:
+        return 'done';
+      default:
+        return 'idle';
+    }
+  }
 }
